7bf95d1496691aee4911ccf141b4743d1f8594f8
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-02-20 17:47:40 +0100

    Adds routing to the project with the use of react-router v6.4+

--- blog-exercise/src/App.tsx
+++ blog-exercise/src/App.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { Navbar } from "./Components/Navbar/Navbar";
+import { Main } from "./Components/Main/Main";
+import { Leaderboards } from "./Components/Leaderboards/Leaderboards";
+import { AddPicture } from "./Components/AddPicture/AddPicture";
+import { ViewYours } from "./Components/ViewYours/ViewYours";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
 
-function App() {
-  return <div className="flex content-center bg-green-400">Hello World!</div>;
+export default function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Main />} />
+        <Route path="leaderboards" element={<Leaderboards />} />
+        <Route path="addpicture" element={<AddPicture />} />
+        <Route path="viewyours" element={<ViewYours />} />
+      </Route>
+    )
+  );
+
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
-export default App;
+export function Root() {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+}
--- blog-exercise/src/Components/Navbar/Navbar.tsx
+++ blog-exercise/src/Components/Navbar/Navbar.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 export function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <Link to="/">Main</Link>
+      <Link to="/leaderboards">Leaderboards</Link>
+      <Link to="/addpicture">Add picture</Link>
+      <Link to="/viewyours">View your pictures</Link>
+    </div>
+  );
 }
