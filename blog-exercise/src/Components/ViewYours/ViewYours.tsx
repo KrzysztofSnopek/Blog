136997c67005339a4712c51fa56b337c61ccf958
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-03-27 19:20:10 +0200

    Gets every uploaded image metadata as a value

--- blog-exercise/src/Components/ViewYours/ViewYours.tsx
+++ blog-exercise/src/Components/ViewYours/ViewYours.tsx
@@ -1,17 +1,32 @@
 import { useState, useEffect } from "react";
 import { storage } from "../../firebase";
-import { ref, listAll, getDownloadURL } from "firebase/storage";
+import {
+  ref,
+  listAll,
+  getDownloadURL,
+  getMetadata,
+  FullMetadata,
+} from "firebase/storage";
 import { auth } from "../../firebase";
 
 export function ViewYours() {
   const [pictureList, setPictureList] = useState([]);
+  const [oMetadata, setOMetadata] = useState<FullMetadata>();
 
   const userName = auth.currentUser?.displayName;
-  const pictureListRef = ref(storage, `projectFiles/${userName}`);
+  const pictureListRef = ref(storage, "projectFiles/");
+
+  // const obtainedMetadata = getMetadata(pictureListRef).then((metadata) =>
+  //   setOMetadata(metadata)
+  // );
+  // console.log(obtainedMetadata);
 
   useEffect(() => {
     let subscribed = true;
     listAll(pictureListRef).then((response) => {
+      response.items.forEach((item) => {
+        getMetadata(item).then((data) => setOMetadata(data));
+      });
       response.items.forEach((item) => {
         getDownloadURL(item).then((url) => {
           if (subscribed) {
@@ -24,6 +39,8 @@ export function ViewYours() {
       subscribed = false;
     };
   }, []);
+
+  console.log(oMetadata?.customMetadata.createdBy);
 
   return (
     <div>
