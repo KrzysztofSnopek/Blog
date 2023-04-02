9f18569f7c7bb46fb958b9f9a6549099387fc89c
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-04-02 20:11:24 +0200

    Adds background gray multiply blend

--- blog-exercise/src/Components/Auth/Auth.tsx
+++ blog-exercise/src/Components/Auth/Auth.tsx
@@ -5,9 +5,9 @@ export const Auth = () => {
   const AuthStore = useContext(AuthStoreContext);
 
   return (
-    <div className="bg-pattern-main h-screen bg-cover">
+    <div className="bg-pattern-main h-screen bg-cover bg-blend-multiply bg-slate-500">
       <div className="flex justify-center items-center min-h-screen">
-        <div className="w-1/4 h-1/2 bg-slate-50 flex flex-col bg-opacity-20 backdrop-blur-md rounded-md drop-shadow-sm">
+        <div className="w-1/4 h-1/2 bg-slate-50 flex flex-col bg-opacity-20 backdrop-blur-md rounded-2xl drop-shadow-lg">
           <p className="text-orange-200 p-6 pt-16 text-center">
             Sign in with Google to continue.
           </p>
