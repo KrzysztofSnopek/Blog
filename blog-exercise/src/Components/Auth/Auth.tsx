e2d9f127ac996022726fdd7f5e4ceccc1cd8e9b7
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-04-02 19:59:22 +0200

    Adds glass effect to the card

--- blog-exercise/src/Components/Auth/Auth.tsx
+++ blog-exercise/src/Components/Auth/Auth.tsx
@@ -5,9 +5,9 @@ export const Auth = () => {
   const AuthStore = useContext(AuthStoreContext);
 
   return (
-    <div className="bg-pattern-main h-screen">
+    <div className="bg-pattern-main h-screen bg-cover">
       <div className="flex justify-center items-center min-h-screen">
-        <div className="w-1/4 h-1/2 bg-slate-50 flex flex-col">
+        <div className="w-1/4 h-1/2 bg-slate-50 flex flex-col bg-opacity-20 backdrop-blur-md rounded-md drop-shadow-sm">
           <p className="text-orange-200 p-6 pt-16 text-center">
             Sign in with Google to continue.
           </p>
