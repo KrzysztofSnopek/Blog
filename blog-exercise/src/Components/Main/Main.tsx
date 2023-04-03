9ff42641c67218e4c8045c2361c09cf3cf382a9d
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-04-03 20:58:42 +0200

    Adds pictures miniatures inside containers in the Main component

--- blog-exercise/src/Components/Main/Main.tsx
+++ blog-exercise/src/Components/Main/Main.tsx
@@ -29,13 +29,13 @@ export function Main() {
 
   return (
     <div>
-      {userName}
-      <div className="flex flex-wrap flex-row-3 bg-slate-400 justify-center gap-4">
+      <div className="bg-slate-400">{userName}</div>
+      <div className="flex flex-wrap flex-row-3 bg-slate-400 justify-center gap-6">
         {pictureList.map((url) => {
           return (
-            <div className="w-1/4 p-8 flex justify-center">
+            <div className="w-1/4 p-8 flex justify-center max-h-96 bg-slate-600 bg-opacity-20 backdrop-blur-md shadow-xl">
               <img
-                className="object-cover max-w-full h-auto"
+                className="object-contain max-w-full h-auto"
                 src={url}
                 key={url}
                 alt=""
