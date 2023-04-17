df306ad390a6640d7cd98280ab32c6973e8eac25
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-04-17 17:03:29 +0200

    Resolves the problem with displaying double images with useEffect

--- blog-exercise/src/Components/ViewYours/ViewYours.tsx
+++ blog-exercise/src/Components/ViewYours/ViewYours.tsx
@@ -1,12 +1,6 @@
 import { useState, useEffect } from "react";
 import { storage } from "../../firebase";
-import {
-  ref,
-  listAll,
-  getDownloadURL,
-  getMetadata,
-  FullMetadata,
-} from "firebase/storage";
+import { ref, listAll, getDownloadURL, getMetadata } from "firebase/storage";
 import { auth } from "../../firebase";
 
 export interface UploadedImage {
@@ -16,43 +10,54 @@ export interface UploadedImage {
 
 export function ViewYours() {
   const [pictureList, setPictureList] = useState<UploadedImage[]>([]);
-  const [imageAlt, setImageAlt] = useState<FullMetadata>();
+  const [userName, setUserName] = useState<string | "unknown">("unknown");
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  const userName = auth.currentUser?.displayName;
   const pictureListRef = ref(storage, `projectFiles`);
 
-  const regex = /\/([^\/-]+)_/;
+  const regex = /\/([^/-]+)_/;
 
   useEffect(() => {
+    const imageData: UploadedImage[] = [];
+
     listAll(pictureListRef).then((response) => {
-      response.items.forEach((item) => {
-        if (item.fullPath.match(regex)?.at(1) === userName) {
-          getMetadata(item).then((metadata) => {
-            setImageAlt(metadata);
-          });
-          const alt = imageAlt?.customMetadata?.imageName;
-          getDownloadURL(item).then((url) => {
-            setPictureList((prev): any => {
-              return [...prev, { url, alt }];
-            });
-          });
-        }
+      const userName = auth.currentUser?.displayName ?? "unknown";
+      setUserName(userName);
+
+      const promises = response.items
+        .filter((item) => item.fullPath.match(regex)?.at(1) === userName)
+        .map((item) => Promise.all([getDownloadURL(item), getMetadata(item)]));
+
+      Promise.all(promises).then((results) => {
+        results.forEach(([url, metadata]) => {
+          const alt = metadata?.customMetadata?.imageName ?? "";
+          imageData.push({ url, alt });
+        });
+
+        setPictureList(imageData);
+        setIsLoading(false);
       });
     });
   }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="bg-slate-400">
       {userName}
       <div className="flex flex-wrap flex-row-3 bg-slate-400 justify-center gap-6">
-        {pictureList.map((item) => {
+        {pictureList.map((item, index) => {
           return (
-            <div className="w-1/4 p-8 flex justify-center flex-col max-h-96 bg-slate-600 bg-opacity-20 backdrop-blur-md shadow-xl">
+            <div
+              className="w-1/4 p-8 flex justify-center flex-col max-h-96 bg-slate-600 bg-opacity-20 backdrop-blur-md shadow-xl"
+              key={`${index}-${item.url}`}
+            >
               <div className="px-6 pt-6 text-center">{item.alt}</div>
               <img
                 className="object-contain max-h-full max-w-full p-6"
                 src={item.url}
-                key={item.url}
                 alt={item.alt}
               />
             </div>
