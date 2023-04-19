f08668729ea599fc7bd84ddd88a6b7c212c23916
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-04-19 18:19:57 +0200

    Adds the feature of viewing images in full screen mode

--- blog-exercise/src/Components/Main/Main.tsx
+++ blog-exercise/src/Components/Main/Main.tsx
@@ -3,11 +3,14 @@ import { storage } from "../../firebase";
 import { ref, listAll, getDownloadURL, getMetadata } from "firebase/storage";
 import { Loader } from "../../Helpers/Loader";
 import { UploadedImage } from "../ViewYours/ViewYours";
+import DisabledByDefaultOutlinedIcon from "@mui/icons-material/DisabledByDefaultOutlined";
 
 export function Main() {
   const [pictureList, setPictureList] = useState<UploadedImage[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const pictureListRef = ref(storage, `projectFiles`);
+  const [isImgFullScreen, setIsImgFullScreen] = useState<boolean>(false);
+  const [tempImgURL, setTempImgURL] = useState<string>("");
 
   useEffect(() => {
     const imageData: UploadedImage[] = [];
@@ -33,17 +36,43 @@ export function Main() {
     return <Loader />;
   }
 
+  const getImg = (imgUrl: string) => {
+    setTempImgURL(imgUrl);
+    setIsImgFullScreen(true);
+  };
+
   return (
     <div>
+      <div
+        className={
+          isImgFullScreen
+            ? "w-full h-screen fixed top-0 left-0 flex justify-center items-center bg-slate-900 bg-opacity-20 backdrop-blur-xl shadow-xl z-10"
+            : "hidden"
+        }
+      >
+        <img
+          src={tempImgURL}
+          className="cursor-pointer"
+          alt=""
+          onClick={() => setIsImgFullScreen(false)}
+        />
+        <div
+          className="cursor-pointer text-slate-600 hover:text-orange-500 fixed top-4 right-4"
+          onClick={() => setIsImgFullScreen(false)}
+        >
+          <DisabledByDefaultOutlinedIcon fontSize="large" color="inherit" />
+        </div>
+      </div>
       <div className="flex flex-wrap flex-row-3 bg-slate-400 justify-center gap-6">
         {pictureList.map((item, index) => {
           return (
             <div
-              className="w-1/4 p-8 flex justify-center max-h-96 bg-slate-600 bg-opacity-20 backdrop-blur-md shadow-xl"
+              className="w-1/4 p-8 flex justify-center max-h-96 bg-slate-600 bg-opacity-20 backdrop-blur-md shadow-xl cursor-pointer hover:opacity-70"
+              onClick={() => getImg(item.url)}
               key={`${index}-${item.url}`}
             >
               <img
-                className="object-contain max-w-full h-auto cursor-pointer hover:opacity-70"
+                className="object-contain max-w-full h-auto"
                 src={item.url}
                 alt={item.alt}
               />
