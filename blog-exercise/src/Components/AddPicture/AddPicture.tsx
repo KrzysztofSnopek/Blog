9768f5e9ae71974da44b7b9fbe490a584a08d96f
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-05-12 19:38:39 +0200

    Adds preview of uploaded photo with option to close it

--- blog-exercise/src/Components/AddPicture/AddPicture.tsx
+++ blog-exercise/src/Components/AddPicture/AddPicture.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { storage } from "../../firebase";
 import { ref, uploadString } from "firebase/storage";
 import { auth, db } from "../../firebase";
@@ -11,6 +11,7 @@ export const AddPicture = observer(() => {
   const [pictureName, setPictureName] = useState<string>("");
   const userName = auth.currentUser?.displayName ?? "";
   const storagePathElement = userName + "_" + v4();
+  const [picturePreview, setPicturePreview] = useState();
 
   const handleImageUpload = async (e: any) => {
     e.preventDefault();
@@ -33,6 +34,11 @@ export const AddPicture = observer(() => {
       alert("uploaded to the storage");
     });
   };
+  const handlePictureChange = (e: any) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setPicturePreview(e.target.files[0]);
+    }
+  };
 
   const convertFile = (files: FileList | null): void => {
     if (files) {
@@ -47,19 +53,37 @@ export const AddPicture = observer(() => {
     }
   };
 
+  const removeSelectedImage = () => {
+    setPicturePreview(undefined);
+  };
+
   return (
     <div>
       <h1 className="font-bold p-2">Share your pictures with others here!</h1>
 
       <form onSubmit={handleImageUpload} className="p-2 bg-orange-300">
-        <input type="file" onChange={(e) => convertFile(e.target.files)} />
+        <input
+          type="file"
+          id="photo-picker"
+          onChange={(e) => {
+            handlePictureChange(e);
+            convertFile(e.target.files);
+          }}
+        />
         <input
           type="text"
           placeholder="Add picture name"
+          id="photo-name"
           onChange={(e) => setPictureName(e.target.value)}
         />
         <button type="submit">Share your picture!</button>
       </form>
+      {picturePreview && (
+        <div>
+          <img src={URL.createObjectURL(picturePreview)} alt="Thumb" />
+          <button onClick={removeSelectedImage}>Remove This Image</button>
+        </div>
+      )}
     </div>
   );
 });
