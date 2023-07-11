6aaa77b52cd861c945ca76ed1f0d2e87cb988d32
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-07-11 15:00:48 +0200

    Creates an array of images to use in slider

--- blog-exercise/src/Components/Main/Slider.tsx
+++ blog-exercise/src/Components/Main/Slider.tsx
@@ -1,3 +1,12 @@
 export const Slider = () => {
-  return <div>Slider</div>;
+  return (
+    <div className="slider">
+      <div className="slide-track">
+        {/* pierwsze slajdy */}
+        <div className="slide">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
---
+++ blog-exercise/src/Helpers/SliderImages.ts
@@ -0,0 +1,23 @@
+import image1 from "../Styling/Images/butterflies-g37e42af8b_1280.jpg";
+import image2 from "../Styling/Images/cornfield-geaaea814a_1280.jpg";
+import image3 from "../Styling/Images/crimson-collared-tanager-g6c5441ea6_1280.jpg";
+import image4 from "../Styling/Images/european-roe-deer-ga9d21857f_1280.jpg";
+import image5 from "../Styling/Images/fauna-g15040abd7_1280.jpg";
+import image6 from "../Styling/Images/flower-g8aaafba66_1280.jpg";
+import image7 from "../Styling/Images/huddle-of-walrus-g49410c10f_1280.jpg";
+import image8 from "../Styling/Images/iceberg-geafdd7746_1280.jpg";
+import image9 from "../Styling/Images/spring-ga71d0331d_1280.jpg";
+import image10 from "../Styling/Images/wheat-g041122d23_1280.jpg";
+
+export const sliderImages = [
+  { id: 1, src: image1 },
+  { id: 2, src: image2 },
+  { id: 3, src: image3 },
+  { id: 4, src: image4 },
+  { id: 5, src: image5 },
+  { id: 6, src: image6 },
+  { id: 7, src: image7 },
+  { id: 8, src: image8 },
+  { id: 9, src: image9 },
+  { id: 10, src: image10 },
+];
