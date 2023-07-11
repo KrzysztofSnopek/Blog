b72272851f674ee9fdf9387c143b8774821db967
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-07-11 15:09:34 +0200

    Renames images and displays all of them

--- blog-exercise/src/Components/Main/Slider.tsx
+++ blog-exercise/src/Components/Main/Slider.tsx
@@ -1,11 +1,17 @@
+import { sliderImages } from "../../Helpers/SliderImages";
+
 export const Slider = () => {
   return (
     <div className="slider">
       <div className="slide-track">
         {/* pierwsze slajdy */}
-        <div className="slide">
-          <img src="" alt="" />
-        </div>
+        {sliderImages.map((image) => {
+          return (
+            <div className="slide" key={image.id}>
+              <img src={image.src} alt={`Image ${image.id}`} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- blog-exercise/src/Helpers/SliderImages.ts
+++ blog-exercise/src/Helpers/SliderImages.ts
@@ -1,13 +1,13 @@
-import image1 from "../Styling/Images/butterflies-g37e42af8b_1280.jpg";
-import image2 from "../Styling/Images/cornfield-geaaea814a_1280.jpg";
-import image3 from "../Styling/Images/crimson-collared-tanager-g6c5441ea6_1280.jpg";
-import image4 from "../Styling/Images/european-roe-deer-ga9d21857f_1280.jpg";
-import image5 from "../Styling/Images/fauna-g15040abd7_1280.jpg";
-import image6 from "../Styling/Images/flower-g8aaafba66_1280.jpg";
-import image7 from "../Styling/Images/huddle-of-walrus-g49410c10f_1280.jpg";
-import image8 from "../Styling/Images/iceberg-geafdd7746_1280.jpg";
-import image9 from "../Styling/Images/spring-ga71d0331d_1280.jpg";
-import image10 from "../Styling/Images/wheat-g041122d23_1280.jpg";
+import image1 from "../Styling/Images/butterflies.jpg";
+import image2 from "../Styling/Images/cornfield.jpg";
+import image3 from "../Styling/Images/crimson-collared-tanager.jpg";
+import image4 from "../Styling/Images/european-roe-deer.jpg";
+import image5 from "../Styling/Images/squirrel.jpg";
+import image6 from "../Styling/Images/wild-mountain-flower.jpg";
+import image7 from "../Styling/Images/huddle-of-walrus.jpg";
+import image8 from "../Styling/Images/iceberg.jpg";
+import image9 from "../Styling/Images/spring.jpg";
+import image10 from "../Styling/Images/wheat-field.jpg";
 
 export const sliderImages = [
   { id: 1, src: image1 },
