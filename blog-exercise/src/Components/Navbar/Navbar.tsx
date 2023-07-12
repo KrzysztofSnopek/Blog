7cfffda988a56cdd8a20bfae52b86ecbf28484ca
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2023-07-12 19:05:20 +0200

    Adds navbar styles - large screen only

--- blog-exercise/src/Components/Navbar/Navbar.tsx
+++ blog-exercise/src/Components/Navbar/Navbar.tsx
@@ -1,21 +1,63 @@
-import { Link } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { useAuthStore } from "../Auth/AuthStore";
 
 export function Navbar() {
   const AuthStore = useAuthStore();
+  const location = useLocation();
 
   return (
-    <div className="bg-blue-50 p-6 text-lg text-slate-700 font-bold flex justify-between h-20">
+    <div className="bg-blue-50 p-6 text-lg text-slate-700 flex justify-between h-20 font-body">
       <div>
-        <Link to="/">GALLERY</Link>
-        <Link to="/leaderboards">LEADERBOARDS</Link>
+        <NavLink
+          to="/"
+          className={`p-8 ${
+            location.pathname === "/"
+              ? "font-extrabold"
+              : "font-normal hover:underline hover:decoration-double"
+          }`}
+        >
+          GALLERY
+        </NavLink>
+        <NavLink
+          to="/leaderboards"
+          className={`p-8 ${
+            location.pathname === "/leaderboards"
+              ? "font-extrabold"
+              : "font-normal hover:underline hover:decoration-double"
+          }`}
+        >
+          LEADERBOARDS
+        </NavLink>
+      </div>
+      <div
+        className={`border rounded-2xl bg-blue-200 shadow-md hover:scale-125 transition duration-500 hover:shadow-2xl ${
+          location.pathname === "/addpicture"
+            ? "font-extrabold scale-125 shadow-2xl"
+            : "font-normal "
+        }`}
+      >
+        <NavLink to="/addpicture" className="p-8">
+          ADD PICTURE
+        </NavLink>
       </div>
       <div>
-        <Link to="/addpicture">ADD PICTURE</Link>
-      </div>
-      <div>
-        <Link to="/viewyours">VIEW YOURS</Link>
-        <button onClick={AuthStore.signUserOut}>LOG OUT</button>
+        <NavLink
+          to="/viewyours"
+          className={`p-8 ${
+            location.pathname === "/viewyours"
+              ? "font-extrabold"
+              : "font-normal hover:underline hover:decoration-double"
+          }`}
+        >
+          VIEW YOURS
+        </NavLink>
+        <NavLink
+          to="/"
+          onClick={AuthStore.signUserOut}
+          className="p-8 hover:underline hover:decoration-double"
+        >
+          LOG OUT
+        </NavLink>
       </div>
     </div>
   );
